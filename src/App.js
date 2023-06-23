e02ac9855bd554988004931cc48153d28bdaad91
Patrick Munuku <wafulapatrick6@gmail.com>
2023-06-23 04:29:54 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Home from './Home';
+import Cart from './Cart';
 
 function App() {
   return (
     <>
       <Home />
+      <Cart />
     </>
   );
 }
